3179f7c275aba06b98c991c3710021f73f38f9de
Chocolabear <132986509+Cocolabear@users.noreply.github.com>
2023-11-17 02:54:38 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int n;
+    double arr[1000]; 
+    double max=0, sum=0; 
+    scanf("%d",&n);          
+ 
+    for(int i=0; i<n; i++)
+    {
+        scanf("%lf", &arr[i]);   
+        if(arr[i]>max)           
+            max = arr[i];
+    }
+ 
+    for(int i=0; i<n; i++)
+    {         
+        arr[i] = arr[i]/max*100;  
+        sum+=arr[i];               
+    }
+    printf("%.2lf", sum/(double)n); 
+}
