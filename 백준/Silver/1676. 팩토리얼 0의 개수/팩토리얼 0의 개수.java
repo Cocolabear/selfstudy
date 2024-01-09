c121787223873244209752788505ae9c84f2d5fa
Chocolabear <132986509+Cocolabear@users.noreply.github.com>
2024-01-09 23:42:56 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 216 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+ 
+		int num = in.nextInt();
+		int cnt = 0;
+ 
+		while (num >= 5) {
+			cnt += num/5;
+			num = num/5;
+		}
+		System.out.println(cnt);
+	}
+ 
+}
