35d2c9f4ab2303e0c0162e2fea3c9b6ca2e07993
Chocolabear <132986509+Cocolabear@users.noreply.github.com>
2025-01-03 04:37:49 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+        loop:
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i] + arr[j] == sum - 100){
+                    arr[i] = 0;
+                    arr[j] = 0;
+                    break loop;
+                }
+            }
+        }
+        Arrays.sort(arr);
+        for(int i=2; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
