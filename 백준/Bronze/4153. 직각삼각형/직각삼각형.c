82bbd9b692c448be468727b0e905df01ce75b048
Chocolabear <132986509+Cocolabear@users.noreply.github.com>
2026-03-27 12:12:44 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+    int a,b,c,tp=0,i=0;
+    while(1)
+    {
+        scanf("%d %d %d",&a,&b,&c);
+        if(a==0 && b==0 && c==0)
+            break;
+        else
+            a=a*a;
+            b=b*b;
+            c=c*c;
+            if(a+b==c || b+c==a || a+c==b)
+                printf("right\n");
+            else if(a+b!=c)
+                printf("wrong\n");
+    }
+}
