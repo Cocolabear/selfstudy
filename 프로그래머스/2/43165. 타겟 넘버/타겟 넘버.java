2157fe0ca478ab1a330ec529441e6dfcd0775c15
Chocolabear <132986509+Cocolabear@users.noreply.github.com>
2026-05-24 14:34:29 +0900

    [level 2] Title: 타겟 넘버, Time: 2.90 ms, Memory: 60.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,20 @@
+class Solution
+{
+    int answer = 0;
+
+    public int solution(int[] numbers, int target) {
+        DFS(numbers, target, 0, 0);
+        return answer;
+    }
+
+    public void DFS(int[] numbers, int target, int depth, int sum) {
+        if(depth == numbers.length) 
+        {
+            if(sum == target)
+                answer++;
+            return;
+        }
+        DFS(numbers, target, depth + 1, sum + numbers[depth]);
+        DFS(numbers, target, depth + 1, sum - numbers[depth]);
+    }
+}
