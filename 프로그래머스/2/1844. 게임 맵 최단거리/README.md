79474f9cb791b85c1bf1442855777532af6bb62b
Chocolabear <132986509+Cocolabear@users.noreply.github.com>
2026-06-05 23:36:24 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.65 ms, Memory: 58.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 57.3 MB, 시간: 4.75 ms
+메모리: 58.5 MB, 시간: 4.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 24일 21:25:46
+2026년 06월 05일 23:36:26
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,54 +1,47 @@
 import java.util.*;
 
-class Solution 
-{
-    static class Node
-    {
-        int x;
-        int y;
+class Solution {
+    public int solution(int[][] map) {
+        int n = map.length;
+        int m = map[0].length;
 
-        Node(int x, int y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    public int solution(int[][] maps)
-    {
-        int n = maps.length;
-        int m = maps[0].length;
+        // 상, 하, 좌, 우 이동을 위한 방향 배열
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
 
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(0, 0));
+        // 큐에는 [x좌표, y좌표]가 순서대로 들어감
+        Queue<int[]> queue = new LinkedList<>();
         
-        while(!queue.isEmpty())
-        {
-            Node now = queue.poll();
-            for(int i = 0; i < 4; i++)
-            {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
-                if(nx < 0 || ny < 0 ||
-                   nx >= n || ny >= m)
-                {
-                    continue;
-                }
-                if(maps[nx][ny] == 0)
-                    continue;
-                if(maps[nx][ny] > 1)
-                    continue;
+        // 시작점 (0, 0) 삽입
+        queue.offer(new int[]{0, 0});
 
-                maps[nx][ny] = maps[now.x][now.y] + 1;
-                queue.offer(new Node(nx, ny));
+        while (!queue.isEmpty())
+        {
+            // 현재 위치를 꺼냄.. now[0]은 x, now[1]은 y로
+            int[] now = queue.poll();
+            int x = now[0];
+            int y = now[1];
+
+            // 4방향 탐색
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                //맵을 벗어났거나, 벽(0)이거나, 이미 방문한 길(>1)이면 패스
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m || map[nx][ny] != 1)
+                    continue;
+                // 다음 칸에 (현재 칸의 거리 + 1)을 저장하여 움직인 거리를 누적
+                map[nx][ny] = map[x][y] + 1;
+
+                // 다음 칸의 좌표를 큐에 넣음
+                queue.offer(new int[]{nx, ny});
             }
         }
-        
-        if(maps[n - 1][m - 1] == 1)
+
+        // 목적지(우측 하단)의 값이 처음 그대로 1이라면, 아무도 도달하지 못했다는 뜻이므로 -1 리턴
+        if (map[n - 1][m - 1] == 1)
             return -1;
-        
-        return maps[n - 1][m - 1];
+        // 최종 도달 거리를 리턴
+        return map[n - 1][m - 1];
     }
 }
